cee95ff44050bd20445a290c345ef1d1e6428c4a
Nathaniel Meyer <nathaniel.meyer@lethbridgecollege.ca>
2021-12-02 13:04:11 -0700

    Add project files.

---
+++ Instructor.h
@@ -0,0 +1,6 @@
+#pragma once
+class Instructor :
+    public Person
+{
+};
+
---
+++ Person.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+class Person {
+public:
+	std::string sNumber;
+	std::string name;
+};
+
